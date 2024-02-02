a18074898f907b20a5cd7a9102dc2bc227b7e6f6
Felpopinho <felipetomaz2609@gmail.com>
2024-02-02 11:18:30 -0300

    All comit

--- BuiltterPC/src/Menu.jsx
+++ BuiltterPC/src/Menu.jsx
@@ -2,17 +2,31 @@ export function Menu(){
 
     return(<>
         <div className="nav_container">
+            <h1>BuillterPC</h1>
 
-            <div className="logo">
-                <h1>BuillterPC</h1>
+            <div className="conta_container">
+                <img src="" />
+            </div>
+        </div>
+
+        <div className="menu_container">
+
+            <div className="menu_esquerdo_container">
+
+                <h1>Conheça a BuiltterPC</h1>
+                <p>diversas funcionalidades disponiveis para facilitar suas experiências com hardware!</p>
+
+                <div>
+                    <button className="btn_create">Criar conta</button>
+                    <button className="btn_enter">Fazer login</button>
+                </div>
+
             </div>
 
-            <ul>
-                <li><a href="#Sobre">Sobre</a></li>
-                <li><a href="#Simulacao">Simulação</a></li>
-                <li><a href="#Promocoes">Promoções</a></li>
-                <li><a href="#Forum">Forum</a></li>
-            </ul>
+            <div className="menu_direito_container">
+
+            </div>
+
         </div>
     </>
 
