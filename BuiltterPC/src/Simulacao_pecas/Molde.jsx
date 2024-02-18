d9897e0a42807e327959be01e8a5f1b4cc403bad
Felpopinho <felipetomaz2609@gmail.com>
2024-02-18 09:34:19 -0300

    a

--- BuiltterPC/src/Simulacao_pecas/Molde.jsx
+++ BuiltterPC/src/Simulacao_pecas/Molde.jsx
@@ -2,7 +2,7 @@ import { Typography, Box } from "@mui/material";
 
 export function Molde(props){
 
-    return <Box sx={{cursor: 'pointer'}} onClick={}>
+    return <Box sx={{cursor: 'pointer'}}>
         <img src={props.simulacao_imagem} className="molde_image"/>
         <Typography variant="h4" fontWeight={600} textAlign={'center'}>{props.simulacao_nome}</Typography>
     </Box>
