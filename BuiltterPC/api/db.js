186ba8a64b3313a9678ea271036dc7438388dad1
Felpopinho <felipetomaz2609@gmail.com>
2024-06-24 14:39:41 -0300

    all commit

--- BuiltterPC/api/db.js
+++ BuiltterPC/api/db.js
@@ -1,7 +1,7 @@
 import mysql from "mysql";
 
 export const db = mysql.createConnection({
-    host: "localhost",
+    host: "builtterpc.vercel.app",
     user: "root",
     password: "Fel_popinho2609",
     database: "crudbuiltterpc"
