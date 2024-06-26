93d416dc43a9a9a0af6770b4000708a4884e5a14
Felpopinho <felipetomaz2609@gmail.com>
2024-06-26 07:21:07 -0300

    all commit

--- BuiltterPC/server.js
+++ BuiltterPC/server.js
@@ -6,7 +6,7 @@ import videoRoutes from "./api/routes/videos.js";
 import produtoRoutes from "./api/routes/produtos.js";
 import simulacaoRoutes from "./api/routes/simulacoes.js";
 import forumRoutes from "./api/routes/comentarios.js";
-import { configDotenv } from "dotenv";
+import 'dotenv/config'
 
 const app = express();
 
