9eef02805008d61b36d749ca0f6ccd1dea56114d
Felpopinho <felipetomaz2609@gmail.com>
2024-06-26 06:52:36 -0300

    all commit

--- BuiltterPC/server.js
+++ BuiltterPC/server.js
@@ -6,14 +6,12 @@ import videoRoutes from "./api/routes/videos.js";
 import produtoRoutes from "./api/routes/produtos.js";
 import simulacaoRoutes from "./api/routes/simulacoes.js";
 import forumRoutes from "./api/routes/comentarios.js";
-import 'dotenv/config'
 
 const app = express();
 
 const port = process.env.PORT || 3000
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}))
 app.use(cors());
 
 app.use("/", userRoutes);
@@ -22,6 +20,4 @@ app.use("/", produtoRoutes);
 app.use("/", simulacaoRoutes);
 app.use("/", forumRoutes);
 
-ViteExpress.listen(app, port, () =>{
-    console.log(port)
-});
+ViteExpress.listen(app, port);
