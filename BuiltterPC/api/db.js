7b1b88565731ddab0176f088dbab1a830252d137
Felpopinho <felipetomaz2609@gmail.com>
2024-10-16 02:59:28 -0300

    a

--- BuiltterPC/api/db.js
+++ BuiltterPC/api/db.js
@@ -1,17 +1,9 @@
 import mysql from "mysql2"
 
-export const db = mysql.createPool({
+export const db = mysql.createConnection({
     host: "bhkcblgonvrzmbiawbut-mysql.services.clever-cloud.com",
     user: "uzafcrmlhdppkmek",
     password: "AUFoDFchry9QRg7R8OZS",
     database: "bhkcblgonvrzmbiawbut",
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
 });
-
-db.getConnection((err, conn) => {
-    if(err) console.log(err)
-    console.log("Connected successfully")
-})
 
