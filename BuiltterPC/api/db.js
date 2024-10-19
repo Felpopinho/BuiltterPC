b0591b7fee684e93c0ce4609e82aa974bf9e7d2a
Felpopinho <felipetomaz2609@gmail.com>
2024-10-18 22:18:58 -0300

    all commit

--- BuiltterPC/api/db.js
+++ BuiltterPC/api/db.js
@@ -1,12 +1,12 @@
-import mysql from 'mysql'
+//import mysql from 'mysql'
 import 'dotenv/config'
-
-export const db = mysql.createConnection({
-    host     : process.env.DB_HOST,
-    database : process.env.DB_NAME,
-    user     : process.env.DB_USER,
-    password : process.env.DB_PASSWORD
-});
+//
+//export const db = mysql.createConnection({
+//    host     : process.env.DB_HOST,
+//    database : process.env.DB_NAME,
+//    user     : process.env.DB_USER,
+//    password : process.env.DB_PASSWORD
+//});
 
 //export const db = new Sequelize('mysql://uzafcrmlhdppkmek:AUFoDFchry9QRg7R8OZS@bhkcblgonvrzmbiawbut-mysql.services.clever-cloud.com:3306/bhkcblgonvrzmbiawbutnpm')
 //
@@ -23,6 +23,25 @@ export const db = mysql.createConnection({
 //    password: "AUFoDFchry9QRg7R8OZS",
 //    database: "bhkcblgonvrzmbiawbut",
 //});
+
+import mysql from 'mysql2'
+
+export const db = mysql.createPool({
+    host: process.env.DB_HOST, 
+    user: process.env.DB_USER, 
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 5,
+    queueLimit: 0
+});
+
+db.getConnection((err, conn) => {
+    if (err) {
+        return console.log(err)
+    } 
+    console.log("Connected successfully")
+})
 
 //mysql://uzafcrmlhdppkmek:AUFoDFchry9QRg7R8OZS@bhkcblgonvrzmbiawbut-mysql.services.clever-cloud.com:3306/bhkcblgonvrzmbiawbutnpm
 
