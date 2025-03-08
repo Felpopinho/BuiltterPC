4df87ec64499c87bdeaac436c9aeb51ddb584420
Felpopinho <felipetomaz2609@gmail.com>
2025-03-07 22:04:49 -0300

    all commit

--- BuiltterPC/api/db.js
+++ BuiltterPC/api/db.js
@@ -1,11 +1,15 @@
-import mysql from 'mysql'
+import mysql from 'mysql2'
 import 'dotenv/config'
 
-export const db = mysql.createConnection({
+export const db = mysql.createPool({
     host     : process.env.DB_HOST,
     database : process.env.DB_NAME,
     user     : process.env.DB_USER,
-    password : process.env.DB_PASSWORD
+    password : process.env.DB_PASSWORD,
+    waitForConections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+
 });
 
 db.connect();
