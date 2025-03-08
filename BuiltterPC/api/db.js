7cd5cfd4d8411a5a1e75107705c1b80e2068e587
Felpopinho <felipetomaz2609@gmail.com>
2025-03-07 21:43:08 -0300

    all commit

--- BuiltterPC/api/db.js
+++ BuiltterPC/api/db.js
@@ -2,10 +2,10 @@ import mysql from 'mysql'
 import 'dotenv/config'
 
 export const db = mysql.createConnection({
-    host     : "bhkcblgonvrzmbiawbut-mysql.services.clever-cloud.com",
-    database : "bhkcblgonvrzmbiawbut",
-    user     : "uzafcrmlhdppkmek",
-    password : "AUFoDFchry9QRg7R8OZS"
+    host     : process.env.DB_HOST,
+    database : process.env.DB_NAME,
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASSWORD
 });
 
 db.connect();
@@ -14,11 +14,5 @@ db.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
   if (err) throw err;
   console.log('Database connected');
 });
-
-//DB_HOST="localhost"
-//DB_NAME="crudbuiltterpc"
-//DB_USER="root"
-//DB_PASSWORD="Fel_*******09"
-//DB_PORT="3300"
 
 
