01a5f54aff0df4b8c31578376f2d2bf4059c5e8c
Felpopinho <felipetomaz2609@gmail.com>
2025-03-10 00:16:58 -0300

    a

--- BuiltterPC/api/server.js
+++ BuiltterPC/api/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import ViteExpress from "vite-express";
 import cors from 'cors';
 import multer from "multer";
-import { serverRouter } from 'server-router.js'
+import { serverRouter } from './routes/server-router.js'
 import 'dotenv/config'
 
 const app = express();
