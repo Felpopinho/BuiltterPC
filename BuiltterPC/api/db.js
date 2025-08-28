b312adec6ce658ce049acfc01204146ca7742e0f
Felpopinho <157515144+Felpopinho@users.noreply.github.com>
2025-08-28 03:30:03 +0000

    a

--- BuiltterPC/api/db.js
+++ BuiltterPC/api/db.js
@@ -7,7 +7,10 @@ export const db = mysql.createPool({
   password: process.env.DB_PASSWORD,                
   database: process.env.DB_NAME,                     
   port: process.env.DB_PORT,                             
-  ssl: { rejectUnauthorized: false }
+  ssl: { rejectUnauthorized: false },
+  connectionLimit: 20,
+  waitForConnections: true,
+  queueLimit: 0,
 });
 
 db.getConnection((err) => {
