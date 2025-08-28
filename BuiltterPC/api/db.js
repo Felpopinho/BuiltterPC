19f96b010d7a4eea20b4192ac1f0290f77a566eb
Felpopinho <157515144+Felpopinho@users.noreply.github.com>
2025-08-28 03:28:35 +0000

    a

--- BuiltterPC/api/db.js
+++ BuiltterPC/api/db.js
@@ -1,43 +1,20 @@
 import mysql from "mysql2/promise";
 import 'dotenv/config'
 
-export default async function db(req, res) {
-  try {
-    const connection = await mysql.createConnection({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,                         
-      password: process.env.DB_PASSWORD,                
-      database: process.env.DB_NAME,                     
-      port: process.env.DB_PORT,                             
-      ssl: { rejectUnauthorized: false }
-    });
+export const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,                         
+  password: process.env.DB_PASSWORD,                
+  database: process.env.DB_NAME,                     
+  port: process.env.DB_PORT,                             
+  ssl: { rejectUnauthorized: false }
+});
 
-    const [rows] = await connection.execute("SELECT NOW() AS data");
-    await connection.end();
-
-    res.status(200).json({ sucesso: true, data: rows[0].data });
-  } catch (error) {
-    console.error("Erro de conexão:", error);
-    res.status(500).json({ sucesso: false, erro: error.message });
+db.getConnection((err) => {
+  if (err) {
+    console.error("Erro ao conectar:", err);
+    return;
   }
-}
-//export const db = mysql.createPool({
-//   host: 
-//    database: 
-//    user: 
-//    password: 
-//    connectionLimit: 20,
-//    waitForConnections: true,
-//    queueLimit: 0,
-//    port: 
-//});
-//
-//db.getConnection((err) => {
-//  if (err) {
-//    console.error("Erro ao conectar:", err);
-//    return;
-//  }
-//  console.log("Conectado ao MySQL");
-//});
-//
-//
+  console.log("Conectado ao MySQL");
+});
+
