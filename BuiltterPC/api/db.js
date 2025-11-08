7b40fa13d141709529a2fbe8e83293e4eda77469
Felopinho <felipetomaz2609@gmail.com>
2025-11-08 15:07:11 -0300

    Refactor Firebase initialization to use environment variables for service account configuration

--- BuiltterPC/api/db.js
+++ BuiltterPC/api/db.js
@@ -1,8 +1,24 @@
 import admin from "firebase-admin"
-import serviceAccountKey from './serviceAccountKey.json' with { type: 'json' }
+
+const privateKey64 = process.env.ENCODE_PRIVATE_KEY
+const privateKey = Buffer.from(privateKey64, 'base64').toString('ascii');
+
+const serviceAccount = {
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: privateKey.replace(/\\n/g, '\n'),
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+  universe_domain: process.env.UNIVERSE_DOMAIN
+}
 
 const firebaseApp = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccountKey),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
